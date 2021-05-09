7bb498babcb49d747ac1d83fce00d08feaeb8c23
Brian Lai <brianlai30@outlook.com>
2021-05-09 15:48:14 -0700

    refactor: update Program.cs to use top-level statements

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,41 +1,23 @@
 using System;
-using System.Threading.Tasks;
+using AsukaApi;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 
-namespace AsukaApi
-{
-    public static class Program
+Console.WriteLine(DateTime.UtcNow.ToString("R"));
+Console.WriteLine(Environment.ProcessId);
+
+var host = Host
+    .CreateDefaultBuilder(args)
+    .UseSerilog()
+    .ConfigureWebHostDefaults(webBuilder =>
     {
-        public static void Main(string[] args) => MainAsync(args).GetAwaiter().GetResult();
+        webBuilder.UseStartup<Startup>();
+    })
+    .Build();
 
-        private static async Task MainAsync(string[] args)
-        {
-            Console.WriteLine(DateTime.UtcNow.ToString("R"));
-            Console.WriteLine(Environment.ProcessId);
+// Configure services.
+// using var scope = host.Services.CreateScope();
+// var services = scope.ServiceProvider;
 
-            var host = CreateHostBuilder(args).Build();
-
-            using var scope = host.Services.CreateScope();
-            var services = scope.ServiceProvider;
-            // Configure services.
-
-            await host.RunAsync();
-        }
-
-        private static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            var host = Host
-                .CreateDefaultBuilder(args)
-                .UseSerilog()
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-
-            return host;
-        }
-    }
-}
+await host.RunAsync();
